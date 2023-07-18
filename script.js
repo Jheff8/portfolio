7b75b83f2370f0af8f71170eea0b719bb05d5c7a
Jhefferson Muzy <jheffersonmuzy46@gmail.com>
2023-07-18 17:23:02 -0300

    Add: Added new projects section with carousel
    Added a new section with a carousel to showcase some of my projects, but the projects themselves are not yet included.

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const navLink = document.querySelectorAll('span.links')
 const header = document.querySelector('header')
 const mainElements = Array.from(document.querySelectorAll('.main-element'))
 
+// This list contains informations about each carousel's elements. It will be used to load the carousel's displayed element informations
+const listOfElementsInformations = [
+    {titleText: 'Mackbook 1', elementDescription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, id magni, officiis quae similique nemo non, quisquam deserunt tempora earum nam cum!", siteLink:'https://github.com/Jheff8'},
+    {titleText: 'Mackbook 2', elementDescription: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorme lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorme lorem lorem lorem lorem", siteLink: 'https://github.com/Jheff8'},
+    {titleText: 'Mackbook 3', elementDescription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, id magni, officiis quae similique nemo non, quisquam deserunt tempora earum nam cum!", siteLink: 'https://github.com/Jheff8'}
+]
+
 // These variables are used to store the timeout IDs returned by setTimeout. They will be used later to clear the timeouts if needed.
 let navVisibilityTimeout1
 let navVisibilityTimeout2
@@ -62,6 +69,15 @@ function toggleNavVisibility() {
     }
 }
 
+function loadCarouselInfo(index) {
+    const siteTitle = document.querySelector('.site-title')
+    const siteDescription = document.querySelector('.site-description')
+    const siteLink = document.querySelector('.site-link')
+    siteTitle.innerHTML = listOfElementsInformations[index].titleText
+    siteDescription.innerHTML = listOfElementsInformations[index].elementDescription
+    siteLink.setAttribute('href', listOfElementsInformations[index].siteLink)
+}
+
 //This interval checks which element is on display by checking which nav has the attr "aria-current" equal to "page" and starts the background animation accordingly 
 let lastItemDisplayed
 setInterval(() => {
@@ -108,7 +124,34 @@ mainElements.forEach(element => {
     observer.observe(element)
 })
 
-// This is resposible for setting and initializing the background animation
+// Initialize the carousel and set some configuration about it
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    effect: 'slide',
+    mousewheel: {
+        forceToAxis: true
+    },
+    grabCursor: true,
+    speed: 800,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+})
+
+// This is called every time the user moves the carousel
+swiper.on('slideChange', function() {
+    // It sends the new "realIndex", which represents what element is being displayed on the carousel, to "loadCarouselInfo()", so it load all the informations about the new element 
+    loadCarouselInfo(this.realIndex)  
+})
+
+// This is resposible for setting and initializing the background animation and loading the carousel's displayed element informations
 window.onload = function() {
     Particles.init({
         selector: '.animated-bg',
@@ -116,7 +159,8 @@ window.onload = function() {
         speed: 0.08,
         color: '#652AB8',
         sizeVariations: 4.5
-      })
+    })
+    loadCarouselInfo(swiper.realIndex)
 }
 
 navbarTogglerBtn.addEventListener('click', changeHamburgerControl)
