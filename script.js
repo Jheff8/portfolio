faf256fd6d9e26e3b6361d7edb0d2d9503f7e9fa
Jhefferson Muzy <jheffersonmuzy46@gmail.com>
2023-07-27 13:53:11 -0300

    Style: Make navigation responsive on larger devices and necessary adjustments.
    Ensured the responsiviness of the navigation (nav) on larger devices. Made essential updates to the HTML, CSS and JavaScript code to ensure proper functionality and bug-free experience.

--- script.js
+++ script.js
@@ -36,42 +36,61 @@ function changeActiveAnchor(index) {
 }
 
 function scrollToSection(sectionId) {
+    const body = document.body
     const section = document.getElementById(sectionId)
-    // Create an interval that runs every second to check if the 'body' element doesn't have the class 'disable-scrolling'. If it doesn't have the class, the scroll position will be changed accordingly to the user's previous choice.
-    const intervalId = setInterval(function() {
-        const body = document.body
-        if (!body.classList.contains('disable-scrolling')) {
-            if (section) {
-                section.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                })
+    if (body.offsetWidth < 768) {
+        // Create an interval that runs every second to check if the 'body' element doesn't have the class 'disable-scrolling'. If it doesn't have the class, the scroll position will be changed accordingly to the user's previous choice.
+        const intervalId = setInterval(function() {
+            if (!body.classList.contains('disable-scrolling')) {
+                if (section) {
+                    section.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    })
+                }
+                clearInterval(intervalId)
             }
-          clearInterval(intervalId)
-        }
-    }, 1000)
+        }, 1000)
+    } else {
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
 }
 
 function toggleNavVisibility() {
+    const body = document.body
     const languageControl = document.querySelector('.language')
     const nav = document.querySelector('nav')
-    clearTimeout(navVisibilityTimeout1)
-    clearTimeout(navVisibilityTimeout2)
 
-    if (hamburgerControl.checked) {
-        header.style.paddingBottom = '95vh'
-        navVisibilityTimeout1 = setTimeout(() => {
-            nav.style.display = 'block'
-        }, 300)
-        navVisibilityTimeout2 = setTimeout(() => {
-            languageControl.style.display = 'flex'
-        }, 700)
-    } else {
-        header.style.paddingBottom = '10px'
-        languageControl.style.display = 'none'
-        navVisibilityTimeout1 = setTimeout(() => {
-            nav.style.display = 'none'
-        }, 400)
+    if (body.offsetWidth < 768) {
+        clearTimeout(navVisibilityTimeout1)
+        clearTimeout(navVisibilityTimeout2)
+        if (hamburgerControl.checked) {
+            body.classList.add('disable-scrolling')
+            header.style.paddingBottom = '95vh'
+            navVisibilityTimeout1 = setTimeout(() => {
+                nav.style.display = 'block'
+            }, 300)
+            navVisibilityTimeout2 = setTimeout(() => {
+                languageControl.style.display = 'flex'
+            }, 700)
+        } else {
+            body.classList.remove('disable-scrolling')
+            header.style.paddingBottom = '10px'
+            languageControl.style.display = 'none'
+            navVisibilityTimeout1 = setTimeout(() => {
+                nav.style.display = 'none'
+            }, 400)
+        }
+    } else{
+        // This is necessary to avoid a bug in case the page's width increases while the header is open
+        if (header.style.paddingBottom == '95vh') {
+            header.style.paddingBottom = '10px'
+            hamburgerControl.checked = false
+            body.classList.remove('disable-scrolling')
+        }
     }
 }
 
@@ -128,7 +147,7 @@ const observer = new IntersectionObserver((entries) => {
 }, {
     // Set the threshold value to trigger visibility detection
     // A value of 0.5 means at least 50% of the target element needs to be visible
-    threshold: 0.5 
+    threshold: 0.5
 })
 
 // Register main elements for observation
@@ -191,14 +210,22 @@ window.onload = function() {
     })
     loadCarouselInfo(swiper.realIndex)
 }
-window.addEventListener('resize', applyStyleBasedOnSize)
+window.addEventListener('resize', () =>{
+    applyStyleBasedOnSize()
+    // Calling the function "toggleNavVisibility" always when the window width changes is necessary to avoid possible bugs on the header.
+    toggleNavVisibility()
+})
 
 navbarTogglerBtn.addEventListener('click', changeHamburgerControl)
 
 navLink.forEach(element => {
     element.addEventListener('click', function() {
+        const body = document.body
         const section = this.innerText.replace(' ', '').toLowerCase()
-        changeHamburgerControl()
+        if (body.offsetWidth < 768) {
+            // The function "changeHamburguerControl" doesn't need to be called if the body's width is equal to or higher than 768 because it means that the hamburger is no longer appearing.
+            changeHamburgerControl()
+        }
         scrollToSection(section)
     })
 })
